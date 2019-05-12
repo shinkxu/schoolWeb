e92f8512681f16e979ff551e7fffedac7a097eb5
岳乃杉 <763839213@qq.com>
2019-05-12 14:44:12 +0800

    修改配置项

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
 module.exports = {
     publicPath: '', // 部署域名的子路径，尽量避免相对路径
     outputDir: 'dist', // 打包后生成的文件目录
-    lintOnSave: process.env.NODE_ENV !== 'production',
+    lintOnSave: false,
+    // lintOnSave: process.env.NODE_ENV !== 'production',
     devServer: {
         overlay: {
-            warnings: true,
-            errors: true
+            warnings: false,
+            errors: false
         },
         host: '0.0.0.0',
         port: '4001' // 监听端口
